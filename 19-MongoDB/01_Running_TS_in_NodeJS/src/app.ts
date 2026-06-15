d18ea93585caa28a74be6c0702eece57349333b9
onureredo <onureredo@yahoo.com>
2026-06-15 15:20:25 +0200

    ts in node

---
+++ 19-MongoDB/01_Running_TS_in_NodeJS/src/app.ts
@@ -0,0 +1,6 @@
+import { pickRandom } from '#utils';
+
+console.log(pickRandom([1, 2, 3, 4, 5]));
+console.log(pickRandom(['apple', 'banana', 'cherry']));
+console.log(pickRandom([true, false, true]));
+console.log(pickRandom(['red', 'green', 'blue', 'yellow']));
